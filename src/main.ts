58b3d00115036187c3bb98bf74550d95c96d9046
y_hokkey <hokkey@users.noreply.github.com>
2019-11-24 17:39:58 +0900

    Change CSS value

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,9 @@ const GRAMMARLY_URL = "https://www.grammarly.com/signin?allowUtmParams=true";
 const GOOGLE_TRANSLATOR_URL = "https://translate.google.com/";
 const GOOGLE_TRANSLATOR_HEIGHT = 320;
 const GOOGLE_TRANSLATOR_CUSTOM_CSS = `
-  header.gb_la { display: none !important; }
-  .frame { border-top: 3px solid black !important; padding-bottom: 66px !important; }
+  body > header { display: none !important; }
+  .frame { border-top: 3px solid black !important; padding-bottom: 66px !important; padding-top: 15px !important; }
+  .input-button-container { display: none !important; }
 `;
 
 app.on("ready", () => {
