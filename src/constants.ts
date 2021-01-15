b9afbff9607dfe01a3d25a6d5ca479fa4392b4f2
y_hokkey <hokkey@users.noreply.github.com>
2021-01-15 18:05:10 +0900

    Update CSS to support the changed google translate page

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,9 @@ export const GRAMMARLY_URL = "https://www.grammarly.com/signin?allowUtmParams=tr
 export const GOOGLE_TRANSLATOR_URL = "https://translate.google.com/";
 export const GOOGLE_TRANSLATOR_HEIGHT = 320;
 export const GOOGLE_TRANSLATOR_CUSTOM_CSS = `
-  body > header { display: none !important; }
-  .frame { border-top: 3px solid black !important; padding-bottom: 66px !important; padding-top: 15px !important; }
-  .input-button-container { display: none !important; }
+  body { border-top: 3px solid black; }
+  body header,
+  body > c-wiz > div > div:first-child,
+  body > c-wiz > div > div > c-wiz > div:first-child { display: none !important; }
+  body > c-wiz > div > div > c-wiz > div:nth-child(2) { padding-top: 8px; }
 `;
